f1d604ab9031aae1626033521468adc0cb5da88c
jameskoss14 <43121875+jameskoss14@users.noreply.github.com>
2018-11-09 16:36:03 -0800

    Updated fragments

--- app/src/main/java/com/example/jameskoss/cosc341_project/MainViews.java
+++ app/src/main/java/com/example/jameskoss/cosc341_project/MainViews.java
@@ -1,7 +1,12 @@
 package com.example.jameskoss.cosc341_project;
 
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainViews extends AppCompatActivity {
 
@@ -9,5 +14,33 @@ public class MainViews extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_views);
+    }
+
+    public void changeFragment(View v){
+        Fragment frag;
+
+        if(v==findViewById(R.id.dayButton)){
+            frag = new DayFragment();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.FragmentPlacement, frag);
+            ft.commit();
+        }
+
+        if(v==findViewById(R.id.weekButton)){
+            frag = new WeekFragment();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.FragmentPlacement, frag);
+            ft.commit();
+        }
+
+        if(v==findViewById(R.id.monthButton)){
+            frag = new MonthFragment();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.FragmentPlacement, frag);
+            ft.commit();
+        }
     }
 }
